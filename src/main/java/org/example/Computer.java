3d0b88b922f547418f54ba4e740aad1fe044453f
Parkjyun <jaeyun0507@naver.com>
2022-12-26 05:57:23 +0900

    first commit

---
+++ src/main/java/org/example/Computer.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import camp.nextstep.edu.missionutils.*;
+public class Computer {
+
+    public static int cpuNumber() {
+
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import camp.nextstep.edu.missionutils.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
